a5a940ff6ae4f6be97a003836110becff077d035
chutong <chutong@domob.cn>
2016-06-22 19:36:44 +0800

    update

--- fabfile.py
+++ fabfile.py
@@ -17,7 +17,22 @@ def git_commit(branch="master", message="update"):
 	local('git push origin %s' % branch)
 
 def scp_from_remote(*files):
-	[ get(os.path.join(base_dir, file), file) for file in files ]
+	cur_dir = os.getcwd()
+	for file in files:
+		total_file_path = file.rsplit('/', 1)
+		if len(total_file_path) > 1:
+			file_path, file_name = total_file_path[0], total_file_path[1]
+		else:
+			file_path, file_name = '', total_file_path[0]
+		get(os.path.join(base_dir, file), os.path.join(cur_dir, file_path))
+	#[ get(os.path.join(base_dir, file), file) for file in files ]
 
 def scp_to_remote(*files):
-	[ put(file, os.path.join(base_dir, file)) for file in files ]
+	for file in files:
+		total_file_path = file.rsplit('/', 1)
+		if len(total_file_path) > 1:
+			file_path, file_name = total_file_path[0], total_file_path[1]
+		else:
+			file_path, file_name = '', total_file_path[0]
+		put(file, os.path.join(base_dir, file_path))
+	#[ put(file, os.path.join(base_dir, file)) for file in files ]
