a4194f1a220ec01a7520b335d313ab6173620818
chutong <chutong@domob.cn>
2016-11-08 10:48:36 +0800

    更新exp;添加测试用例

--- exp_manager/exp_manager.py
+++ exp_manager/exp_manager.py
@@ -12,133 +12,200 @@ try:
 except ImportError:
 	import configparser as ConfigParser
 	import xml.etree.ElementTree as ET
+import copy
+
+
+class Exp(object):
+	"""
+	负责当前Exp检查
+	"""
+	def __init__(self, exp_id, name, description):
+		self._exp_id = int(exp_id)
+		self._name = name
+		self._description = description
+		self._campaign_ids = []
+		self._account_ids = []
+		self._user_ids = []
+		self._campaign_end_numbers = []
+
+	@property
+	def id(self):
+		return self._exp_id
+
+	@property
+	def name(self):
+		return self._name
+
+	@property
+	def user_ids(self):
+		return self._user_ids
+
+	@user_ids.setter
+	def user_ids(self, value):
+		self._user_ids = value
+
+	@property
+	def account_ids(self):
+		return self._account_ids
+
+	@account_ids.setter
+	def account_ids(self, value):
+		self._account_ids = value
+
+	@property
+	def campaign_ids(self):
+		return self._campaign_ids
+
+	@campaign_ids.setter
+	def campaign_ids(self, value):
+		self._campaign_ids = value
+
+	@property
+	def campaign_end_numbers(self):
+		return self._campaign_end_numbers
+
+	@campaign_end_numbers.setter
+	def campaign_end_numbers(self, value):
+		self._campaign_end_numbers = value
+
+	def has_user(self, user_info={}):
+		try:
+			user_attribute_list = ['user_id', 'account_id', 'campaign_id', 'campaign_end_number']
+			user_info_n = copy.copy(user_info)
+			user_info_n['campaign_end_number'] = user_info_n['campaign_id'] % 10
+			for user_attribute in user_attribute_list:
+				class_attribute = '_' + user_attribute + 's'
+				if user_info_n[user_attribute] in self.__dict__[class_attribute]:	
+					return True
+			return False
+		except KeyError as e:
+			raise e
 
 
 class ExpManager(object):
 	"""
-	 负责AAM实验策略的加载和管理
+	 负责实验策略的加载和管理
 	"""
-	exp_campaigns_dict = {}
-	exp_accounts_dict = {}
-	exp_campaign_end_dict = {}
-	exp_id_dict = {}
+	strategy_info_dict = {}
 
-	@classmethod
-	def get_exp_id(cls, strategy_name, logger):
-		return cls.exp_id_dict[strategy_name] if cls.strategy_exists(strategy_name) else 0
-
-	@classmethod
-	def strategy_exists(cls, strategy_name, logger):
-		return strategy_name in cls.exp_id_dict
-
-	@classmethod
-	def account_strategy_exists(cls, strategy_name, logger):
-		''' 指定名称的策略是否存在'''
-		return strategy_name in cls.exp_accounts_dict
-
-	@classmethod
-	def campaign_strategy_exists(cls, strategy_name, logger):
-		''' 指定名称的策略是否存在'''
-		return strategy_name in cls.exp_campaigns_dict
-
-	@classmethod
-	def account_in_strategy(cls, strategy_name, fb_account_id, logger):
-		''' campaign存在于strategy生效的campaign列表'''
-		if cls.account_strategy_exists(strategy_name, logger):
-			if -1 in cls.exp_accounts_dict[strategy_name]: # all account used for this strategy
-				logger.debug('strategy account valid for all')
-				return True
-			else:
-				logger.debug('strategy account ids %s' % str(cls.exp_accounts_dict[strategy_name]))
-				return fb_account_id in cls.exp_accounts_dict[strategy_name]
+	def __init__(self, strategy):
+		self._strategy = strategy
+		self._origin_handlers = []
+	
+	def __enter__(self):
+		logger = logging.getLogger(__name__)
+		if self._strategy in self.strategy_info_dict:
+			exp = self.strategy_info_dict[self._strategy]
+			self._origin_handlers = logger.handlers
+			logger.handlers = []
+			ch = logging.StreamHandler(sys.stdout)
+			formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(expid)s - %(expname)s')
+			ch.setFormatter(formatter)
+			logger.addHandler(ch)
+			extra = {'expid':exp.id, 'expname':exp.name}
+			logger = logging.LoggerAdapter(logger, extra)
+			return (exp, logger)
 		else:
-			logger.debug('strategy %s not defined' % strategy_name)
-			return False
+			logger.error('strategy %s not defined in exp, error' % self._strategy)
+			return (None, logger)
 
-	@classmethod
-	def campaign_in_strategy(cls, strategy_name, campaign_id, logger):
-		''' campaign存在于strategy生效的campaign列表'''
-		if cls.campaign_strategy_exists(strategy_name, logger):
-			if -1 in cls.exp_campaigns_dict[strategy_name]: # all campaign used for this strategy
-				logger.debug('strategy campaign valid for all')
-				return True
-			else:
-				logger.debug('strategy campaign ids %s' % str(cls.exp_campaigns_dict[strategy_name]))
-				return campaign_id in cls.exp_campaigns_dict[strategy_name]
-		else:
-			logger.debug('strategy %s not defined' % strategy_name)
-			return False
-
-	@classmethod
-	def campaign_end_number_in_strategy(cls, strategy_name, campaign_id, logger):
-		''' campaign存在于strategy生效的campaign列表'''
-		if cls.strategy_exists(strategy_name, logger):
-			end_numbers = cls.exp_campaign_end_dict[strategy_name]
-			end_number_flag = False
-			for end_number in end_numbers:
-				if not ((campaign_id - end_number) % 10):
-					end_number_flag = True
-					break
-			return end_number_flag
-		else:
-			logger.error('strategy %s not defined' % strategy_name)
-			return False
+	def __exit__(self, exc_type, exc_value, exc_tb):
+		logger = logging.getLogger(__name__)
+		logger.handlers = self._origin_handlers
 
 	@classmethod
 	def load(cls, filename, logger):
 		""" 加载策略文件"""
 		try:
 			tree = ET.ElementTree(file=filename)
+			root = tree.getroot()
 		except IOError, e:
-			logger.exception(e)
+			raise e
 		else:
 			try:
 				exp = tree.getroot()
 				for strategy in exp:
-					name = strategy.find('name').text
-					exp_id = strategy.find('exp_id').text
-					campaign_info = strategy.find('campaigns')
-					if campaign_info:
-						campaigns = [ int(campaign) for campaign in campaign_info.text.split(',') ]
-						cls.exp_campaigns_dict.setdefault(name, campaigns)
-					account_info = strategy.find('accounts')
-					if account_info is not None:
-						accounts = [ int(account) for account in account_info.text.split(',') ]
-						cls.exp_accounts_dict.setdefault(name, accounts)
-					campaign_end_info = strategy.find('campaign_end_numbers')
-					if campaign_end_info is not None:
-						campaign_end_numbers = [ int(end_number) for end_number in campaign_end_info.text.split(',') ]
-						cls.exp_campaign_end_dict.setdefault(name, campaign_end_numbers)
-					cls.exp_id_dict.setdefault(name, int(exp_id))
-				logger.info('load account_info %s' % str(cls.exp_accounts_dict))
-				logger.info('load campaign_info %s' % str(cls.exp_campaigns_dict))
-				logger.info('load campaign_end_info %s' % str(cls.exp_campaign_end_dict))
-				logger.info('load exp_id %s' % str(cls.exp_id_dict))
+					exp_id, name, desc = -1, '', ''
+					user_ids, account_ids, campaign_ids, campaign_end_numbers = [], [], [], []
+					for elem in strategy.iter():
+						if elem.tag == 'name':
+							name = elem.text
+						if elem.tag == 'exp_id':
+							exp_id = elem.text
+						if elem.tag == 'desc':
+							description = elem.text
+						if elem.tag == 'users':
+							user_ids = [ int(user) for user in elem.text.split(',') ]
+						if elem.tag == 'accounts':
+							account_ids = [ int(account) for account in elem.text.split(',') ]
+						if elem.tag == 'campaigns':
+							campaign_ids = [ int(campaign) for campaign in elem.text.split(',') ]
+						if elem.tag == 'campaign_end_numbers':
+							campaign_end_numbers = [ int(campaign_end_number) for campaign_end_number in elem.text.split(',') ]
+					exp = Exp(exp_id, name, description)
+					exp.user_ids = user_ids
+					exp.account_ids = account_ids
+					exp.campaign_ids = campaign_ids
+					exp.campaign_end_numbers = campaign_end_numbers
+
+#					campaign_info = strategy.find('campaigns')
+#					if campaign_info is not None:
+#						exp.campaign_ids = [ int(campaign) for campaign in campaign_info.text.split(',') ]
+#					account_info = strategy.find('accounts')
+#					if account_info is not None:
+#						exp.account_ids = [ int(account) for account in account_info.text.split(',') ]
+#					campaign_end_info = strategy.find('campaign_end_numbers')
+#					if campaign_end_info is not None:
+#						exp.campaign_end_numbers = [ int(end_number) for end_number in campaign_end_info.text.split(',') ]
+					cls.strategy_info_dict[name] = exp
 			except Exception, e:
-				logger.exception(e)
+				raise e
 
 
 if __name__ == '__main__':
-
-	# test ExpManager
 	basepath = os.path.abspath(os.getcwd())
-	filename = os.path.join(basepath, 'conf/dummy_exp.xml')
+	logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
+		format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+	)
+	logger = logging.getLogger(__name__)
 
-	logging.basicConfig(filename=os.path.join(basepath, 'logs/mock.log'), level=logging.DEBUG,
-		format = '[%(filename)s:%(lineno)s - %(funcName)s %(asctime)s;%(levelname)s] %(message)s',
-		datefmt = '%a, %d %b %Y %H:%M:%S'
-		)
-	logger = logging.getLogger('ExpManager')
-
+	filename = os.path.join(basepath, 'conf/test_exp.xml')
 	try:
-		filename = '123'
 		ExpManager.load(filename, logger)
-		print ExpManager.campaign_in_strategy('Mock', 1234, logger)
-		print ExpManager.campaign_in_strategy('Mock', 12345, logger)
-		print ExpManager.campaign_in_strategy('Mock2', 1234, logger)
-		print ExpManager.campaign_in_strategy('Mock2', 12345, logger)
-		print ExpManager.account_in_strategy('Mock2', 12345, logger)
-		print ExpManager.campaign_end_number_in_strategy('TimeWithCPA', 19875, logger)
-		print ExpManager.campaign_end_number_in_strategy('TimeWithCPA', 19878, logger)
-	except Exception, e:
+	except IOError as e:
 		logger.exception(e)
+	else:
+		logger.info('work starts')
+		# usage
+		with ExpManager("Mock") as (exp, logger):
+			from collections import OrderedDict
+			user_info = OrderedDict(user_id=1, account_id=123, campaign_id=1234)
+			if exp and exp.has_user(user_info):
+				# main logic
+				print 'user exists in exp'
+				logger.debug('user_id[%d] account_id[%d] campaign_id[%d] participate exp' % tuple(user_info.values()))
+			else:
+				print 'user not exists in exp'
+				logger.debug('user_id[%d] account_id[%d] campaign_id[%d] dont participate exp' % tuple(user_info.values()))
+
+			user_info = OrderedDict(user_id=1, account_id=111, campaign_id=1111)
+			if exp and exp.has_user(user_info):
+				# main logic
+				print 'user exists in exp'
+				logger.debug('user_id[%d] account_id[%d] campaign_id[%d] participate exp' % tuple(user_info.values()))
+			else:
+				print 'user not exists in exp'
+				logger.debug('user_id[%d] account_id[%d] campaign_id[%d] dont participate exp' % tuple(user_info.values()))
+
+		# usage
+		with ExpManager("Mock2") as (exp, logger):
+			from collections import OrderedDict
+			user_info = OrderedDict(user_id=1, account_id=123, campaign_id=1234)
+			if exp and exp.has_user(user_info):
+				# main logic
+				print 'user exists in exp'
+				logger.debug('user_id[%d] account_id[%d] campaign_id[%d] participate exp' % tuple(user_info.values()))
+			else:
+				print 'user not exists in exp'
+				logger.debug('user_id[%d] account_id[%d] campaign_id[%d] dont participate exp' % tuple(user_info.values()))
+		logger.info('work ends')
---
+++ exp_manager/old_exp_manager.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+# vim: set bg=dark noet ts=4 sw=4 fdm=indent :
+
+__author__ = 'linpingta@163.com'
+
+import os
+import sys
+import logging
+try:
+	import ConfigParser
+	import xml.etree.cElementTree as ET
+except ImportError:
+	import configparser as ConfigParser
+	import xml.etree.ElementTree as ET
+
+
+class ExpManager(object):
+	"""
+	 负责AAM实验策略的加载和管理
+	"""
+	exp_campaigns_dict = {}
+	exp_accounts_dict = {}
+	exp_campaign_end_dict = {}
+	exp_id_dict = {}
+
+	@classmethod
+	def get_exp_id(cls, strategy_name, logger):
+		return cls.exp_id_dict[strategy_name] if cls.strategy_exists(strategy_name) else 0
+
+	@classmethod
+	def strategy_exists(cls, strategy_name, logger):
+		return strategy_name in cls.exp_id_dict
+
+	@classmethod
+	def account_strategy_exists(cls, strategy_name, logger):
+		''' 指定名称的策略是否存在'''
+		return strategy_name in cls.exp_accounts_dict
+
+	@classmethod
+	def campaign_strategy_exists(cls, strategy_name, logger):
+		''' 指定名称的策略是否存在'''
+		return strategy_name in cls.exp_campaigns_dict
+
+	@classmethod
+	def account_in_strategy(cls, strategy_name, fb_account_id, logger):
+		''' campaign存在于strategy生效的campaign列表'''
+		if cls.account_strategy_exists(strategy_name, logger):
+			if -1 in cls.exp_accounts_dict[strategy_name]: # all account used for this strategy
+				logger.debug('strategy account valid for all')
+				return True
+			else:
+				logger.debug('strategy account ids %s' % str(cls.exp_accounts_dict[strategy_name]))
+				return fb_account_id in cls.exp_accounts_dict[strategy_name]
+		else:
+			logger.debug('strategy %s not defined' % strategy_name)
+			return False
+
+	@classmethod
+	def campaign_in_strategy(cls, strategy_name, campaign_id, logger):
+		''' campaign存在于strategy生效的campaign列表'''
+		if cls.campaign_strategy_exists(strategy_name, logger):
+			if -1 in cls.exp_campaigns_dict[strategy_name]: # all campaign used for this strategy
+				logger.debug('strategy campaign valid for all')
+				return True
+			else:
+				logger.debug('strategy campaign ids %s' % str(cls.exp_campaigns_dict[strategy_name]))
+				return campaign_id in cls.exp_campaigns_dict[strategy_name]
+		else:
+			logger.debug('strategy %s not defined' % strategy_name)
+			return False
+
+	@classmethod
+	def campaign_end_number_in_strategy(cls, strategy_name, campaign_id, logger):
+		''' campaign存在于strategy生效的campaign列表'''
+		if cls.strategy_exists(strategy_name, logger):
+			end_numbers = cls.exp_campaign_end_dict[strategy_name]
+			end_number_flag = False
+			for end_number in end_numbers:
+				if not ((campaign_id - end_number) % 10):
+					end_number_flag = True
+					break
+			return end_number_flag
+		else:
+			logger.error('strategy %s not defined' % strategy_name)
+			return False
+
+	@classmethod
+	def load(cls, filename, logger):
+		""" 加载策略文件"""
+		try:
+			tree = ET.ElementTree(file=filename)
+		except IOError, e:
+			logger.exception(e)
+		else:
+			try:
+				exp = tree.getroot()
+				for strategy in exp:
+					name = strategy.find('name').text
+					exp_id = strategy.find('exp_id').text
+					campaign_info = strategy.find('campaigns')
+					if campaign_info:
+						campaigns = [ int(campaign) for campaign in campaign_info.text.split(',') ]
+						cls.exp_campaigns_dict.setdefault(name, campaigns)
+					account_info = strategy.find('accounts')
+					if account_info is not None:
+						accounts = [ int(account) for account in account_info.text.split(',') ]
+						cls.exp_accounts_dict.setdefault(name, accounts)
+					campaign_end_info = strategy.find('campaign_end_numbers')
+					if campaign_end_info is not None:
+						campaign_end_numbers = [ int(end_number) for end_number in campaign_end_info.text.split(',') ]
+						cls.exp_campaign_end_dict.setdefault(name, campaign_end_numbers)
+					cls.exp_id_dict.setdefault(name, int(exp_id))
+				logger.info('load account_info %s' % str(cls.exp_accounts_dict))
+				logger.info('load campaign_info %s' % str(cls.exp_campaigns_dict))
+				logger.info('load campaign_end_info %s' % str(cls.exp_campaign_end_dict))
+				logger.info('load exp_id %s' % str(cls.exp_id_dict))
+			except Exception, e:
+				logger.exception(e)
+
+
+if __name__ == '__main__':
+
+	# test ExpManager
+	basepath = os.path.abspath(os.getcwd())
+	filename = os.path.join(basepath, 'conf/dummy_exp.xml')
+
+	logging.basicConfig(filename=os.path.join(basepath, 'logs/mock.log'), level=logging.DEBUG,
+		format = '[%(filename)s:%(lineno)s - %(funcName)s %(asctime)s;%(levelname)s] %(message)s',
+		datefmt = '%a, %d %b %Y %H:%M:%S'
+		)
+	logger = logging.getLogger('ExpManager')
+
+	try:
+		filename = '123'
+		ExpManager.load(filename, logger)
+		print ExpManager.campaign_in_strategy('Mock', 1234, logger)
+		print ExpManager.campaign_in_strategy('Mock', 12345, logger)
+		print ExpManager.campaign_in_strategy('Mock2', 1234, logger)
+		print ExpManager.campaign_in_strategy('Mock2', 12345, logger)
+		print ExpManager.account_in_strategy('Mock2', 12345, logger)
+		print ExpManager.campaign_end_number_in_strategy('TimeWithCPA', 19875, logger)
+		print ExpManager.campaign_end_number_in_strategy('TimeWithCPA', 19878, logger)
+	except Exception, e:
+		logger.exception(e)
