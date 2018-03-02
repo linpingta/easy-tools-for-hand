14ddaeeb965b5aa8425623cf34cce57da845ab3a
chutong <chutong@chutongdeMacBook-Pro.local>
2018-03-02 18:29:49 +0800

    add pipeline logic

---
+++ pipeline/sample/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__init__.py.py
+
+"""
+
+import os
+import sys
+import logging
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="parse argument")
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
