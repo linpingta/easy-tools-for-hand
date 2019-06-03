b3af94e7c3fc4f6ccf7ee44a4e2d316e5c39fc1a
Tong, Chu <chut1987@gmail.com>
2019-06-03 22:38:00 +0800

    Update utils.py

--- mr_job_template/utils.py
+++ mr_job_template/utils.py
@@ -21,20 +21,6 @@ def run_cmd_silent(cmd, func=None):
     sys.stdout.flush()
     return process.returncode, res
 
-def test_hdfs_file(path):
-    cmd = "%s fs -test -e %s" % (hadoop_bin_path, path)
-    ret, _ = run_cmd_silent(cmd)
-    cmd = "%s fs -test -e %s/_temporary" % (hadoop_bin_path, path)
-    xret, _ = run_cmd_silent(cmd)
-    return True if ret == 0 and xret != 0 else False
-
-def copy_file_to_hdfs(local_path, hdfs_path):
-    if (not hdfs_path) or (not local_path):
-        return False
-    cmd = "%s fs -put %s %s" % (hadoop_bin_path, local_path, hdfs_path)
-    ret, _ = run_cmd_silent(cmd)
-    return ret
-
 def delete_hadoop(output_path):
     if not output_path:
         return False
